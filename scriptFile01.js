368a00d04b020c763a7e616342d3058fd35ea6a4
GlobalRepository <79697704+Nandeeponline@users.noreply.github.com>
2024-07-08 01:20:55 +0530

    Initial Commit

---
+++ scriptFile01.js
@@ -0,0 +1,5 @@
+function onClickMenu(event = undefined) {
+    console.log("Working on click", event);
+    console.log("Selected Page:", event.target.id);
+    window.open("https://www.bing.com/","_self");
+}
